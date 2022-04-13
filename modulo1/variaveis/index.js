bcb8c2a5ec60b20564ceebd3b1abfc6a5d560d38
Otuada <adauto.matosjr@gmail.com>
2022-04-12 22:50:16 -0300

    aula sobre variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,97 @@
+
+/*let a = 10
+let b = 10
+
+console.log(10)
+
+b = 5
+console.log(10, 5) */
+
+
+//2//
+/* let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(10,10 ,10)*/
+
+
+// 3 //
+
+/*let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`) */
+
+/* vc recebe "quanto vc recebe por dia dividido por quantas
+horas vc trabalha por dia" */ //É uma string e nao uma numbers por isso so contera letras//
+
+
+// execicios de escrita de codigo //
+
+//1
+
+//c) R: sem atribuir valor nao podemos descobrir q tipo de variavel é//
+
+let nome = "adauto"
+let idade = 33
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+prompt("Ola usario por acaso seu nome é", nome, "vc teria", idade,"?");
+
+// no meu navegador googleCrome so aparece ate o nome o ("vc teria, idade ?") nao aparece
+
+console.log("Ola", nome, "você tem", idade, "anos.");
+
+//2
+let trabalho = prompt("Vc trabalhou hj");
+
+let sorvete = prompt("Vc tomou sorvete hj");
+
+let abraco = prompt("Eu abracei meu filho hj");
+
+console.log("Voce trabalhou hj,", trabalho);
+console.log("Vc comeu sorvete hj,", sorvete);
+console.log("Eu abracei meu filho hj,", abraco);
+
+//3
+
+let a = 10
+let b = 25
+
+// Aqui faremos uma lógica para trocar os valores
+
+let c = a 
+     a = b
+     b = c
+
+// Depois de trocados, teremos o seguinte resultado:
+console.log("O novo valor de a é", a) // O novo valor de a é 25
+console.log("O novo valor de b é", b) // O novo valor de b é 10
+
+// Desafio
+
+let x = prompt("escolha um numero para representar x");
+
+let y = prompt("escolha um numero para representar y");
+
+
+console.log( x + y );
+console.log(x * y);
+
+
+// quando faço a soma ele imprime a concatenação dos numeros nao sei o q fazer fé em Deus e na crianças da favela
+
+
+
+
+
+
+
+
+
+
+ 
