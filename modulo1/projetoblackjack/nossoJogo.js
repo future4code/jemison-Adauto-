58bce09606dc66b9db3c9e1cfb84a967787685c2
Otuada <adauto.matosjr@gmail.com>
2022-05-03 19:31:07 -0300

    mexer

---
+++ modulo1/projetoblackjack/nossoJogo.js
@@ -0,0 +1,35 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+    //console.log("ola mundo")
+
+console.log ("Bem vindo ao JOGO BLACKJACK!")
+    
+
+if(confirm("Começar novo jogo?")) {
+      console.log("Iniciando rodada de BlackJack!")
+         } else {
+      console.log("O jogo acabou!")
+   }
+
+   ; 
+// Sorteia uma carta. Por exemplo, o rei de ouros
+
+
+// imprime o texto da carta. Nesse caso: "K♦️"
+
+ 
+// imprime o valor da carta (um número). Nesse caso: 10)
+
+
+
