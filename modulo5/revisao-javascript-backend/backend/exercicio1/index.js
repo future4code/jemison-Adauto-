d9c567584e9f467657af5b0e1d2849008e22c46e
Adauto <adauto.matosjr@gmail.com>
2022-10-06 23:44:59 -0300

    exercicio revisao

---
+++ modulo5/revisao-javascript-backend/backend/exercicio1/index.js
@@ -0,0 +1,13 @@
+console.log("exercicio 1");
+
+//transformar C em K e F
+
+const grausCelsius = "5" // escolha um numero
+
+
+    const grausK = Number(grausCelsius) + 273.15
+    const grausF = Number(grausCelsius) * 1.8 + 32
+
+
+
+console.log(grausCelsius +"C em Fahrenheit é " + grausF + "F e em Kelvin é " + grausK +"K")
