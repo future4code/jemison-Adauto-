bed607787af83024c7ff5f475d98302b60950671
Adauto <adauto.matosjr@gmail.com>
2022-10-09 23:00:03 -0300

    exercicio1 0.2

--- modulo5/revisao-javascript-backend/backend/exercicio1/index.js
+++ modulo5/revisao-javascript-backend/backend/exercicio1/index.js
@@ -1,33 +1,11 @@
-console.log("exercicio 1");
-
-//transformar C em K e F
-const grausCelsius = "5" // escolha um numero
+	//exercicio1
 
 
-const grausK = Number(grausCelsius) + 273.15
-const grausF = Number(grausCelsius) * 1.8 + 32
-const texto = console.log(grausCelsius +"C em Fahrenheit é " + grausF + "F e em Kelvin é " + grausK +"K")
-
-
-const resposta = () =>{
-    if(grausCelsius === Number || grausF === Number || grausK === Number )
-    return 
+const checaGrausK = (grausCelsius) => {
+    return Number(grausCelsius) + 273.15
 }
-
-
-
-let escolha = 'K'   //'Digite F ou K para selecionar'
-let numeroCelsius = 25      //'Digite um numero'
-
-const soma = () =>{
-
-    if(escolha === 'K'){
-    const respostaK = numeroCelsios + 273.15
-    return respostaK
-    }else if(escolha === 'F'){
-    const respostaF = numeroCelsios * 9 / 5 + 32
-    return respostaF
-    }
-    
-}
- console.log(soma(numeroCelsius))
+const checaGrausF = (grausCelsius) =>{
+    return Number(grausCelsius) * 1.8 + 32
+}	
+console.log(checaGrausK(12)+"º Kelvin")
+console.log(checaGrausF(32)+"º Fahrenheit")
