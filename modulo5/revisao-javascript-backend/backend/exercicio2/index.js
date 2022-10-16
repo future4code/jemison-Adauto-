dc4eb5c06400bd6a735a7f58edfd61e8797d0c12
Adauto <adauto.matosjr@gmail.com>
2022-10-16 16:39:38 -0300

    revisao

--- modulo5/revisao-javascript-backend/backend/exercicio2/index.js
+++ modulo5/revisao-javascript-backend/backend/exercicio2/index.js
@@ -1,1 +1,51 @@
 console.log("exercício 2");
+
+const pessoas = [
+	{ id: 1, nome: 'Fulano' },
+	{ id: 2, nome: 'Ciclano' },
+	{ id: 3, nome: 'Beltrano' },
+	{ id: 4, nome: 'Fulana' }
+]
+
+const adicionado = pessoas.push({id: 5, nome: 'Guilherme'})
+
+for(pessoa of pessoas){
+	 console.log(pessoa)
+	
+}
+
+ console.log(adicionado)
+
+ console.log(pessoas)
+
+ const validando = () =>{
+ 	const novaPessoa = {id: 4, nome: 'Guilherme'}
+
+ 	pessoas.map(() =>{
+ 		if(pessoas.id === novaPessoa.id || pessoas.nome === novaPessoa.nome){
+ 			return "Já existente!"
+ 		}else{
+ 			const adicionado = pessoas.push(novaPessoa)
+ 			return adicionado
+			 
+ 		}
+
+ 	})
+		
+ }
+
+ console.log(validando())
+
+
+
+
+// 2 tabuada
+
+
+const multiplicando = (num) =>{
+     	
+     	for(let i=0 ; i<=10 ; i++){
+        console.log(i + "x" + num +"=" + (num * i))
+        }
+        }
+     console.log(multiplicando(5))
