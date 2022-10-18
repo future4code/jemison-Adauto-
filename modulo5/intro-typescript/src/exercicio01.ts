7846432bd9c1db1900349babe0875dbc3defa26e
Adauto <adauto.matosjr@gmail.com>
2022-10-18 00:40:49 -0300

    intro typescript

---
+++ modulo5/intro-typescript/src/exercicio01.ts
@@ -0,0 +1,27 @@
+/*export function checaTriangulo(a, b, c) {
+    if (a !== b && b !== c) {
+      return "Escaleno";
+    } else if (a === b && b === c) {
+      return "Equilátero";
+    } else {
+      return "Isósceles";
+    }
+  }
+
+  console.log(checaTriangulo(4, 4, 4))*/
+
+  function cauculaTriangulo(){
+    const ladoA = process.argv[2]
+    const ladoB = process.argv[3]
+    const ladoC = process.argv[4]
+
+    if (ladoA !== ladoB && ladoB !== ladoC) {
+        return "Escaleno";
+      } else if (ladoA === ladoB && ladoB === ladoC) {
+        return "Equilátero";
+      } else {
+        return "Isósceles";
+      }
+  }
+
+  console.log(cauculaTriangulo)
