9c7b76f39aaf475296f710c36f97d4bb6920db60
Adauto <adauto.matosjr@gmail.com>
2022-10-18 23:36:23 -0300

    aprofundamento typescript

---
+++ modulo5/aprofundamento-typescript/src/exercicio2.ts
@@ -0,0 +1,27 @@
+const arrayDeNumeros : number[] = [21, 18, 65, 44, 15, 18]
+
+//A entrada da função é de uma Array, então a função ela é uma array e number...
+
+function obterEstatisticas(numeros: any) {
+
+    const numerosOrdenados: any = numeros.sort(
+        (a:number, b:number) => a - b
+    )
+
+    let soma = 0
+
+    for (let num of numeros) {
+        soma += num
+    }
+
+    const estatisticas : any = {
+        maior: numerosOrdenados[numeros.length - 1],
+        menor: numerosOrdenados[0],
+        media: soma / numeros.length
+    }
+
+
+    return estatisticas
+}
+
+console.log(obterEstatisticas(arrayDeNumeros))
