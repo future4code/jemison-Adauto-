483014842092e5a1e310b3f2c9ceb2fc51ede457
Adauto <adauto.matosjr@gmail.com>
2022-10-25 23:43:08 -0300

    intro-express 1

---
+++ modulo5/intro-express/src/data.ts
@@ -0,0 +1,55 @@
+type Usuario = {
+    id:number,
+    nome:string,
+    telefone:number,
+    website:string,
+    email:string
+}
+
+type PostsUser = {
+   userId:number,
+   id:number,
+   title:string,
+   body:string
+}
+
+export const user: Usuario[] =
+[
+    {
+      id: 1,
+      nome: "Leanne Graham",
+      telefone: 1-770-736-8031 -56442,
+      website: "hildegard.org",
+      email: "Sincero@april.biz",
+    },
+    {
+      id: 2,
+      nome: "Ervin Howell",
+      telefone: 692-65939125,
+      website: "anastasia.net",
+      email: "Shanna@melissa.tv"
+    },
+    {
+      id: 3,
+      nome: "Clementine Bauch",
+      telefone: 1-463-123-4447,
+      website: "ramiro.info",
+      email: "Nathan@yesenia.net"
+    },
+    {
+      id: 4,
+      nome: "Patrícia Lebsack",
+      telefone: 493-170-9623-156,
+      website: "kale.biz",
+      email: "Julianne.OConner@kory.org"
+    },
+    {
+      id: 5,
+      nome: "Chelsey Dietrich",
+      telefone: 254-954-1289,
+      website: "demarco.info",
+      email: "Lucio_Hettinger@annie.ca"
+    },
+  ]
+
+ 
