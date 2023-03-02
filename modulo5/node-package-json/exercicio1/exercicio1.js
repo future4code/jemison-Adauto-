4998f5cdad32a4b6562bd8a7bd1d1988fc97df96
Adauto <adauto.matosjr@gmail.com>
2023-03-02 00:52:08 -0300

    arrumando o conflito de rota no script

--- modulo5/node-package-json/exercicio1/exercicio1.js
+++ modulo5/node-package-json/exercicio1/exercicio1.js
@@ -1,12 +1,13 @@
 // Para acessar os parâmetros: process.argv
 
      const nome = process.argv[2]
-     const idade = Number(process.argv[3])
+     const idade = process.argv[3]
 
-function exercicioLetraBC() {
-     const sete = Number(7) 
+function exercicio1LetraBC() {
+     const sete = 7
      const soma= Number(idade) + Number(sete)
-     
-     return 
      console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${soma}`)
+
 }
+
+exercicio1LetraBC(Adauto 33)
