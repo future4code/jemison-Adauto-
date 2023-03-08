2cc8bd3c184355394112b77c42f43d8d3d1d80e0
Adauto <adauto.matosjr@gmail.com>
2023-03-08 00:14:16 -0300

    exercicio 02 arrumado

--- modulo5/intro-typescript/src/exercicio02.ts
+++ modulo5/intro-typescript/src/exercicio02.ts
@@ -5,13 +5,13 @@
     console.log([cor1, cor2, cor3])
  }*/
 
- const cor1 = process.argv[2]
- const cor2 = process.argv[3]
- const cor3 = process.argv[4]
+ const cor1 = process.argv[2];
+ const cor2 = process.argv[3];
+ const cor3 = process.argv[4];
 
  function imprimeTresCoresFavoritas(){
          
        return  "Minhas cores favoritas são:" + cor1 + " " + cor2 + " " + cor3
     }
     
-    console.log(imprimeTresCoresFavoritas())
+    console.log(imprimeTresCoresFavoritas(cor1, cor2, cor3))
