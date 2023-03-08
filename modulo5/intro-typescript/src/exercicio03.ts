c6964935e9b7268782d153aa92d794c8fd9c0cc3
Adauto <adauto.matosjr@gmail.com>
2023-03-08 00:28:43 -0300

    exercicio03 arrumado

--- modulo5/intro-typescript/src/exercicio03.ts
+++ modulo5/intro-typescript/src/exercicio03.ts
@@ -4,13 +4,13 @@
    return cond1 || cond2
 }*/
 
-const digitado = Number(process.argv[2])
+const ano = Number(process.argv[2]);
 
-function checaAnoBissexto(ano:Number):boolean {
-    const cond1 = ano % 400 === 0
-    const cond2 = (ano % 4 === 0) && (ano % 100 !== 0)
+function checaAnoBissexto(ano) {
+    const cond1 = ano % 400 === 0 ;
+    const cond2 = (ano % 4 === 0) && (ano % 100 !== 0);
     return cond1 || cond2
 }
 
-console.log(checaAnoBissexto(digitado))
+console.log(checaAnoBissexto(ano));
 
