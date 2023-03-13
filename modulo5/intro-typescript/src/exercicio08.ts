b47cd893bc43476c41ea31d74eadd3d10e12c477
Adauto <adauto.matosjr@gmail.com>
2023-03-13 20:08:42 -0300

    desafio 07 concluido

---
+++ modulo5/intro-typescript/src/exercicio08.ts
@@ -0,0 +1,8 @@
+/*Escreva uma função que receba uma string e retorne a string reversa. 
+Em outras palavras, se o input da sua função for "abcd", a saída deve ser 
+"dcba" . */
+
+function stringReversa(){
+
+    return
+}
