61a7a575b09eb7fc00210194021ec61d48fe5dcd
Adauto <adauto.matosjr@gmail.com>
2023-03-31 07:49:42 -0300

    continua

--- modulo5/intro-express/src/index.ts
+++ modulo5/intro-express/src/index.ts
@@ -1,6 +1,6 @@
 import express, {Request, Response} from "express"
-
 import cors from 'cors'
+import { user } from "./data"
 
 const app = express()
 
@@ -8,10 +8,17 @@ app.use(express.json())
 
 app.use(cors())
 
-app.get('/',(req: Request, res: Response)=>{
+app.get('/api',(req: Request, res: Response)=>{
     res.status(200).send("API trabalhando")
 })
 
+app.get('/users',(req: Request, res: Response)=>{
+    const todos = user.map((lista)=>{
+        return lista.id
+    })
+    res.status(201).send(todos)
+})
+
 app.listen(3003, () => {
-    console.log("API rodando na porta 3000")
+    console.log("API rodando na porta 3003")
 })
