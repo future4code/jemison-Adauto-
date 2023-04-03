c88276fe68da83f720d74c708a64e444c635b730
Adauto <adauto.matosjr@gmail.com>
2023-04-02 23:38:05 -0300

    intro express exercicios e desafio completo

--- modulo5/intro-express/src/data.ts
+++ modulo5/intro-express/src/data.ts
@@ -3,7 +3,7 @@ type Usuario = {
     nome:string,
     telefone:number,
     website:string,
-    email:string
+    email:string    
 }
 
 type PostsUser = {
@@ -52,4 +52,98 @@ export const user: Usuario[] =
     },
   ]
 
+
+  export const postDosUsuarios: PostsUser[] =
+  [
+        {
+         userId: 1,
+         id: 1,
+         title: "alo alo",
+         body: "tudo nosso"
+        },
+        {
+         userId: 1,
+         id: 2,
+         title: "aqui e agora",
+         body: "asdasdasdasdasdsa"
+        },
+        {
+         userId: 1,
+         id: 3,
+         title: "asdasd",
+         body: "et"
+        },
+        {
+         userId: 2,
+         id: 4,
+         title: "peace and love",
+         body: "no war"
+        },
+        {
+         userId: 2,
+         id: 5,
+         title: "odio",
+         body: "e amor"
+        },
+        {
+         userId: 2,
+         id: 6,
+         title: "estude",
+         body: "para um futuro melhor"
+        },
+        {
+          userId: 3,
+          id: 7,
+          title: "codando",
+          body: "para saber"
+        },
+        {
+         userId: 3,
+         id: 8,
+         title: "mestre dos magos",
+         body: "caverna do dragão"
+        },
+        {
+         userId: 3,
+         id: 9,
+         title: "rodeio de cotia",
+         body: "super faturamento e desvio de dinheiro"
+        },
+        {
+          userId: 4,
+          id: 10,
+          title: "construir casa",
+          body: "terreno"
+        },
+        {
+         userId: 4,
+         id: 11,
+         title: "clara nunes",
+         body: "canto das 3 raças"
+        },
+        {
+         userId: 4,
+         id: 12,
+         title: "alguma coisa em algum lugar",
+         body: "acalanta meu coraçao"
+        },       
+        {          
+          userId: 5,
+          id: 13,
+          title: "bora meu filho",
+          body: "bora bora abre a porta"
+        },
+        {
+         userId: 5,
+         id: 14,
+         title: "El capone",
+         body: "poderoso chefao"
+        },
+        {
+         userId: 5,
+         id: 15,
+         title: "repteis",
+         body: "teoria da evolução"
+        },
+  ]
  
--- modulo5/intro-express/src/index.ts
+++ modulo5/intro-express/src/index.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response} from "express"
 import cors from 'cors'
 import { user } from "./data"
+import { postDosUsuarios } from "./data"
 
 const app = express()
 
@@ -13,11 +14,49 @@ app.get('/api',(req: Request, res: Response)=>{
 })
 
 app.get('/users',(req: Request, res: Response)=>{
-    const todos = user.map((lista)=>{
-        return lista.id
-    })
-    res.status(201).send(todos)
+    
+    
+    res.status(201).send(user)
 })
+
+
+app.get('/posts',(req: Request, res: Response)=>{
+    
+    
+    res.status(200).send(postDosUsuarios)
+})
+
+app.get('/users/1/posts', (req: Request, res: Response) => {
+    const userPosts = postDosUsuarios.filter(post => post.userId === 1)
+    res.status(200).send(userPosts)
+  })
+  
+app.delete('/posts/:postId', (req: Request, res: Response) => {
+    const postId = Number(req.params.postId)
+    const index = postDosUsuarios.findIndex(post => post.id === postId)
+  
+    if (index === -1) {
+      return res.status(404).send({ message: 'Deu ruim post nao encontrado' })
+    }
+  
+    postDosUsuarios.splice(index, 1)
+  
+    return res.status(204).send(postDosUsuarios)
+  })
+
+app.delete('/deleta/:usuarioId',(req: Request, res: Response) =>{
+    const usuarioId = Number(req.params.usuarioId)
+    const index = user.findIndex(user => user.id === usuarioId)
+   
+    if (index === -1) {
+        return res.status(404).send({ message: 'Ih usuario nao encontrado' })
+      }
+
+      user.splice(index, 1)
+      return res.status(204).send(user)
+
+})  
+  
 
 app.listen(3003, () => {
     console.log("API rodando na porta 3003")
